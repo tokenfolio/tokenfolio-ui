1111fa7bca9d3dd8d1be47260cabc3f36e907c9d
Oluwatobiloba Olalusi <hello@tobiolalusi.com>
2022-03-03 02:41:37 +0100

    defined and configured colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
     fontFamily: {
       'dm-sans': ['DM Sans', 'sans-serif']
     },
+    colors: {
+      cultured: 'var(--color-cultured)',
+      platinum: 'var(--color-platinum)',
+      black: 'var(--color-black)',
+      'pale-taupe': 'var(--color-pale-taupe)',
+      'roman-silver': 'var(--color-roman-silver)',
+      'persian-blue': 'var(--color-persian-blue)',
+      transparent: 'transparent',
+    },
     extend: {},
   },
   plugins: [],
