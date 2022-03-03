db5adc0644974072f73c455de9eb948febb42625
Oluwatobiloba Olalusi <hello@tobiolalusi.com>
2022-03-03 01:48:40 +0100

    main font setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   content: ['./src/**/*.{ts,tsx}', './public/index.html'],
   darkMode: 'class',
   theme: {
+    fontFamily: {
+      'dm-sans': ['DM Sans', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [],
