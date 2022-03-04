13ed9185f1fb1d8244954875f5503e3fbe1ae78a
Oluwatobiloba Olalusi <hello@tobiolalusi.com>
2022-03-04 03:26:31 +0100

    installed auth0 sdk

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
+import { Auth0Provider } from '@auth0/auth0-react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Dashboard from 'pages/dashboard/Dashboard';
 import SignUp from 'pages/signup/SignUp';
 import SignIn from 'pages/signin/SignIn';
 import NotFound from 'pages/404/NotFound';
+import { AUTH0_CONFIG } from './constants';
 
 export const PATH_DASHBOARD = '/dashboard';
 export const PATH_SIGNIN = '/signin';
 export const PATH_SIGNUP = '/signup';
 
 const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route index element={<Navigate to={PATH_DASHBOARD} replace />} />
-      <Route path={PATH_DASHBOARD} element={<Dashboard />} />
-      <Route path={PATH_SIGNIN} element={<SignIn />} />
-      <Route path="/login" element={<Navigate to={PATH_SIGNIN} replace />} />
-      <Route path={PATH_SIGNUP} element={<SignUp />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  </BrowserRouter>
+  <Auth0Provider domain={AUTH0_CONFIG.domain} clientId={AUTH0_CONFIG.clientId} redirectUri={window.location.origin}>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Navigate to={PATH_DASHBOARD} replace />} />
+        <Route path={PATH_DASHBOARD} element={<Dashboard />} />
+        <Route path={PATH_SIGNIN} element={<SignIn />} />
+        <Route path="/login" element={<Navigate to={PATH_SIGNIN} replace />} />
+        <Route path={PATH_SIGNUP} element={<SignUp />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  </Auth0Provider>
 );
 
 export default App;
---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+// eslint-disable-next-line import/prefer-default-export
+export const AUTH0_CONFIG = {
+  domain: 'tokenfolio.eu.auth0.com',
+  clientId: 'NAkEPHFTKDhHKBNY6x1ARb4DErFYQIt3',
+};
