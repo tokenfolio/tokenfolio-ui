67251db5c21e4a91cd4516424a6d3b2a9b79b35a
Oluwatobiloba Olalusi <hello@tobiolalusi.com>
2022-03-06 20:41:27 +0100

    basic info text state
    Signed-off-by: Oluwatobiloba Olalusi <hello@tobiolalusi.com>

--- src/pages/dashboard/account/basic/AccountBasicInfoSettings.tsx
+++ src/pages/dashboard/account/basic/AccountBasicInfoSettings.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const AccountBasicInfoSettings: React.FC = () => {
   const { user } = useAuth0();
+  const [firstName, setFirstName] = useState(user?.given_name);
+  const [lastName, setLastName] = useState(user?.family_name);
+
   return (
     <div className="mx-4">
       <h2>Basic info</h2>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={(e) => e.preventDefault()} className="flex flex-col gap-4">
         <label className="input__lg">
           <div className="label">First name</div>
-          <input type="text" value={user?.given_name} />
+          <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
         </label>
         <label className="input__lg">
           <div className="label">Last name</div>
-          <input type="text" value={user?.family_name} />
+          <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} />
         </label>
         <div className="my-6">
           <button className="button__lg__black" type="submit">
