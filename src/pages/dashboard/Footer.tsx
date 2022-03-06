5abccea0cf81511f59761411fabbdf0b8fd8e757
Oluwatobiloba Olalusi <hello@tobiolalusi.com>
2022-03-06 20:36:52 +0100

    footer text
    Signed-off-by: Oluwatobiloba Olalusi <hello@tobiolalusi.com>

--- src/pages/dashboard/Footer.tsx
+++ src/pages/dashboard/Footer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const Footer: React.FC = () => <footer>Footer</footer>;
+const Footer: React.FC = () => <footer className="text-sm">&copy;Copyright 2022 Tokenfolio</footer>;
 
 export default Footer;
