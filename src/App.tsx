5db57aff5170e62007db4cbc332162f42a8c0984
Oluwatobiloba Olalusi <hello@tobiolalusi.com>
2022-03-06 20:42:10 +0100

    removed redux store provider
    Signed-off-by: Oluwatobiloba Olalusi <hello@tobiolalusi.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Auth0Provider } from '@auth0/auth0-react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import DashboardLayout from 'pages/dashboard/DashboardLayout';
 import Dashboard from 'pages/dashboard/dashboard/Dashboard';
 import Wallet from 'pages/dashboard/wallet/Wallet';
@@ -22,29 +21,26 @@ import {
   PATH_WALLET,
   PATH_SETTINGS_BASIC,
 } from 'shared/constants';
-import store from 'shared/store/store';
 import AccountBasicInfoSettings from 'pages/dashboard/account/basic/AccountBasicInfoSettings';
 
 const App = () => (
   <Auth0Provider domain={AUTH0_CONFIG.domain} clientId={AUTH0_CONFIG.clientId} redirectUri={window.location.origin}>
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path={PATH_DASHBOARD} element={<DashboardLayout />}>
-            <Route index element={<Dashboard />} />
-            <Route path={PATH_WALLET} element={<Wallet />} />
-            <Route path={PATH_ACTIVE_DEALS} element={<ActiveDeals />} />
-            <Route path={PATH_TOKEN_SALES} element={<TokenSales />} />
-            <Route path={PATH_ALLOCATION_STAKING} element={<AllocationStaking />} />
-            <Route path={PATH_SETTINGS} element={<AccountSettingsLayout />}>
-              <Route path={PATH_SETTINGS_BASIC} element={<AccountBasicInfoSettings />} />
-            </Route>
+    <BrowserRouter>
+      <Routes>
+        <Route path={PATH_DASHBOARD} element={<DashboardLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path={PATH_WALLET} element={<Wallet />} />
+          <Route path={PATH_ACTIVE_DEALS} element={<ActiveDeals />} />
+          <Route path={PATH_TOKEN_SALES} element={<TokenSales />} />
+          <Route path={PATH_ALLOCATION_STAKING} element={<AllocationStaking />} />
+          <Route path={PATH_SETTINGS} element={<AccountSettingsLayout />}>
+            <Route path={PATH_SETTINGS_BASIC} element={<AccountBasicInfoSettings />} />
           </Route>
-          <Route path={PATH_LOGOUT} element={<Logout />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+        </Route>
+        <Route path={PATH_LOGOUT} element={<Logout />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   </Auth0Provider>
 );
 
